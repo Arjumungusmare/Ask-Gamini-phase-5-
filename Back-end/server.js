4a2d616e500c404d34d881c3af77c067e649e2e3
Arju Mungusmare <arjumungusmare@gmail.com>
2026-08-18 13:25:39 +0530

    Add root API route

--- Back-end/server.js
+++ Back-end/server.js
@@ -16,6 +16,10 @@ app.use(express.json());
 
 app.use("/api/feedback", feedbackRoutes);
 
+app.get("/", (req, res) => {
+    res.send("API is running");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
